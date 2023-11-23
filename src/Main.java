c69c7539a4b4a44d777c7f3707b094a6d8d8f3a3
vener <vener.gazin@gmail.com>
2023-11-23 22:10:50 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 20000; // стоимость билета
+        int percent = 5;  //
+        int bonus = amount * percent / 100;
+        System.out.println(" Ваш бонус:" + bonus + " мили");
+    }
+}
